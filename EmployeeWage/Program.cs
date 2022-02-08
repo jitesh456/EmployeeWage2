527b8407fecb5fb69f02572c1f6cc1ebda8781db
Jitesh <jitesh0dubey@gmail.com>
2022-02-08 09:33:59 +0530

    ADDED:Feature for partial present check

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,15 +7,18 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == 0)
             {
                 Console.WriteLine("Employee is present");
             }
+            else if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is partial present");
+            }
             else
             {
                 Console.WriteLine("Employee is absent");
-
             }
         }
     }
